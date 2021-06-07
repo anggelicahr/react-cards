f8268b00152c54d77f7b7bf9421928304e83f90e
Angélica Herrera <anggelicahrr@gmail.com>
2021-06-07 18:21:51 -0500

    firrst commit

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Card() {
+    return (
+        <div className="card">
+            <div className="card-body">
+                <h4 className="card-title">My Title</h4>
+            </div>
+            <p className="card-text text-secundary">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit magni incidunt cumque itaque ea sequi est rerum! Ipsum recusandae error tenetur ipsam. Voluptates praesentium reiciendis optio ea est voluptatem omnis!
+            </p>
+        </div>
+    )
+}
+
+export default Card;
